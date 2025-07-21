8be14bd5c4c094c46ab9dda2042bb64c4c50d013
vishal_Sonone <vishaljsonone@gmail.com>
2025-07-21 13:27:55 +0530

    update login api

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,7 +17,8 @@ const Login = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://localhost:8080/api/students/login", {
+     const response = await fetch(`${import.meta.env.VITE_API_URL}/api/students/login`, {
+
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
